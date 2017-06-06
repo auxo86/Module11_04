c8cef1811a9029aad82f3b44593e453df6921e86
auxo86 <auxo86@gmail.com>
2017-06-06 19:21:45 +0800

    <!--mode改成SQLServer並且修改timeout為10分鐘-->     <sessionState mode="SQLServer" timeout="10"                   sqlConnectionString="data source=.\sqlexpress;integrated security=sspi">     </sessionState>

---
+++ Module11_01/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Module11_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/TempDataDemo
+        public ActionResult TempDataDemo()
+        {
+            ViewData["Key1"] = DateTime.Now.ToString();
+            ViewBag.key2 = DateTime.Now.ToString();
+            TempData["Key3"] = DateTime.Now.ToString();
+
+            //return View();
+            return RedirectToAction("TempDataDemo2");
+        }
+
+        // GET: Home/TempDataDemo2
+        public ActionResult TempDataDemo2()
+        {
+            return View();
+        }
+
+        // GET: Home/SessionDemo
+        public ActionResult SessionDemo()
+        {
+            if (Session["count"] == null)
+                Session["count"] = 0;
+            else
+                Session["count"] = (int)Session["count"] + 1;
+
+            return View();
+        }
+    }
+}
